2443b81354588eca507e939b8220c22e13210198
Erhai-lake <erhai.lake.0714@gmail.com>
2026-05-09 17:22:00 +0800

    先让vuepress正常运行, 顺便删掉了被误上传的cache文件夹和Git跟踪

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,12 +1,14 @@
-import { viteBundler } from '@vuepress/bundler-vite'
-import { defineUserConfig } from 'vuepress'
-import { plumeTheme } from 'vuepress-theme-plume'
+import {viteBundler} from "@vuepress/bundler-vite"
+import {defineUserConfig} from "vuepress"
+import {plumeTheme} from "vuepress-theme-plume"
 
 export default defineUserConfig({
-	// 必须设置默认语言
-	lang: 'zh-CN',
-	theme: plumeTheme({
-		// 主题配置...
-	}),
+	base: "/Ponder-Tutorials/",
+	lang: "zh-CN",
+	title: "Ponder-Tutorials",
+	description: "Ponder-Tutorials",
+
 	bundler: viteBundler(),
+	theme: plumeTheme({
+	}),
 })
