2ab415dbc9af2e7ff9cc87eceba26f6596bcbf08
Eternal-Snowstorm <eternalsnowstorm@foxmail.com>
2026-05-09 15:54:57 +0800

    排除

--- docs/.vuepress/.temp/pages/index.html.vue
+++
@@ -1,8 +0,0 @@
-<template><div><p>This is the content of home page. Check <a href="https://vuejs.press/reference/default-theme/frontmatter.html#home-page" target="_blank" rel="noopener noreferrer">Home Page Docs</a> for more details.</p>
-<hr>
-<p>如何启动编辑
-在文件夹目录cmd中输入 npm run docs:dev
-之后就可以在 http://localhost:8080/ 浏览docs</p>
-</div></template>
-
-
